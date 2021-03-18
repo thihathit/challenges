929d493c5fee945f605f102ab0e18395272d3c82
Thiha <developer.thiha@gmail.com>
2021-03-18 20:57:17 +0630

    feat [components]: Add Total Donation Counter block

---
+++ challenge-react/src/components/blocks/TotalDonation/TotalDonation.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { TweenLite, Circ } from 'gsap';
+
+// Elements
+import Loader from '../../elements/Loader/Loader.jsx';
+
+// Styles
+import {
+  Container,
+  Number,
+  NumberTitle,
+  Info,
+} from './TotalDonation.styled.jsx';
+
+const TotalDonation = ({ loading = false, duration = 0.8 }) => {
+  const total = useSelector(({ total_donations }) => total_donations);
+
+  const [currentNumber, setCurrentNumber] = useState(() => total);
+
+  useEffect(() => {
+    const Initial = {
+      target: currentNumber,
+    };
+
+    const Animation = TweenLite.to(Initial, duration, {
+      target: total,
+      roundProps: 'target',
+      ease: Circ.easeInOut,
+      onUpdate() {
+        setCurrentNumber(Initial.target);
+      },
+    });
+
+    return () => {
+      Animation.kill();
+    };
+  }, [total]);
+
+  return (
+    <Container>
+      <Loader active={loading} />
+
+      {!loading && (
+        <Info>
+          <NumberTitle>Total Donations</NumberTitle>
+
+          <Number>THB {currentNumber.toLocaleString('th-TH')}</Number>
+        </Info>
+      )}
+    </Container>
+  );
+};
+
+export default TotalDonation;
---
+++ challenge-react/src/components/blocks/TotalDonation/TotalDonation.styled.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  text-align: center;
+`;
+
+export const Info = styled.nav`
+  display: grid;
+  grid-template-columns: 100%;
+  grid-gap: 5px;
+`;
+
+export const NumberTitle = styled.p`
+  font-size: 0.8em;
+  color: #b3b5b8;
+  font-weight: 600;
+`;
+
+export const Number = styled.p`
+  font-size: 1.5em;
+  color: #244af3;
+  font-weight: 800;
+`;
