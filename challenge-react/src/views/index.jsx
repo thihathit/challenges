fe190e8b7050d1ee401c92e6adaf8286babfd15b
Thiha <developer.thiha@gmail.com>
2021-03-18 21:02:51 +0630

    update [views]: Update Index page

--- challenge-react/src/views/index.jsx
+++ challenge-react/src/views/index.jsx
@@ -1,17 +1,66 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useMemo } from 'react';
+import { useDispatch } from 'react-redux';
+
+// Blocks
+import TotalDonation from '../components/blocks/TotalDonation/TotalDonation.jsx';
+import CharityList from '../components/blocks/Charities/CharityList.jsx';
+import CharityItem from '../components/blocks/Charities/CharityItem/CharityItem.jsx';
 
 // Styles
-import { Container } from './index.styled.jsx';
+import { Container, Seperator, Title } from './index.styled.jsx';
+
+// Api
+import { useDonations } from '../hooks/api.js';
 
 const Index = () => {
-  const Store = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const { state, get: refreshDonations } = useDonations();
+
+  const totalDonationAmount = useMemo(
+    () =>
+      state.get.data.reduce(
+        (accumulator, value) => accumulator + value.amount,
+        0
+      ),
+    [state.get.data]
+  );
+
+  // Fetch on mounted
+  useEffect(() => {
+    refreshDonations();
+  }, []);
+
+  // Sync total amount with redux
+  useEffect(() => {
+    dispatch({ type: 'UPDATE_TOTAL_DONATIONS', value: totalDonationAmount });
+  }, [totalDonationAmount]);
 
   return (
     <Container>
-      <h1>Omise Tamboon React</h1>
+      <Title>Omise Tamboon React</Title>
 
-      {JSON.stringify(Store, null, 2)}
+      <Seperator />
+
+      <TotalDonation loading={state.get.loading} />
+
+      <Seperator visible={false} />
+
+      <CharityList>
+        {({ charities, payments }) =>
+          charities.map((item) => (
+            <CharityItem
+              id={item.id}
+              key={item.id}
+              image={item.image}
+              name={item.name}
+              currency={item.currency}
+              payments={payments}
+              afterPaid={refreshDonations}
+            />
+          ))
+        }
+      </CharityList>
     </Container>
   );
 };
--- challenge-react/src/views/index.styled.jsx
+++ challenge-react/src/views/index.styled.jsx
@@ -1,5 +1,44 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div(`
-  background-color: red;
-`);
+import { screenMax } from '../styles/mixins.styled.jsx';
+
+export const Container = styled.main`
+  display: grid;
+  grid-template-columns: 100%;
+  grid-gap: 20px;
+
+  padding: 80px 20px;
+  box-sizing: content-box;
+`;
+
+export const Seperator = styled.div`
+  width: 100%;
+  height: 2px;
+  margin: 0 auto;
+  max-width: 35px;
+  border-radius: 100px;
+  background-color: #d0d1d3;
+
+  ${({ visible = true }) => css`
+    visibility: ${visible ? 'visible' : 'hidden'};
+  `}
+`;
+
+export const Title = styled.h1`
+  text-align: center;
+  color: #687186;
+  font-size: 2.5em;
+  font-weight: 600;
+
+  ${screenMax(700, () => {
+    return css`
+      font-size: 2.3em;
+    `;
+  })}
+
+  ${screenMax(550, () => {
+    return css`
+      font-size: 2em;
+    `;
+  })}
+`;
